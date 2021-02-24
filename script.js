bc04a5a45fd34cfe477ca7e6e415e4d33b83ede0
Leonardo <leonardofabbri125@gmail.com>
2021-02-24 17:16:27 +0100

    definitivo

---
+++ script.js
@@ -0,0 +1,18 @@
+var cognomi= ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi" ]
+console.log (cognomi);
+
+
+var cognome=prompt("INSERISCI IL TUO COGNOME");
+cognomi.push(cognome);
+
+
+var cognomi_bis= ["Bianchi","Rossi","Duzioni","Balsano","Verdi",cognome]
+console.log (cognomi_bis);
+cognomi_bis.sort();
+
+cognomi_bis[0]==1;
+
+
+
+
+
